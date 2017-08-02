8feee6de349c01fa8d41eed1e59677a910720430
Mikaeil Orfanian <mikaeil.orfanian@toreforge.com>
2017-08-02 18:07:56 +0200

    turn test db to singleton so that its data is shared among all modules

--- gateway/__init__.py
+++ gateway/__init__.py
@@ -3,5 +3,24 @@ import os
 db_type = os.environ.get('DB_ENGINE', 'nosql')
 app_env = os.environ.get('APP_ENVIRONMENT', 'test')
 
+
+class Borg:
+    _shared_state = {}
+
+    def __init__(self):
+        self.__dict__ = self._shared_state
+
+
+class DB(Borg):
+    def __init__(self):
+        Borg.__init__(self)
+
+    def __getitem__(self, x):
+        return self.__dict__[x]
+
+    def __setitem__(self, key, value):
+        self.__dict__[key] = value
+
+
 if db_type == 'nosql' and app_env == 'test':
-    db = dict()
+    db = DB()
