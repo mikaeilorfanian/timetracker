9aa0250c39dac166efd70ad15f9a0e57964d029f
Mikaeil Orfanian <mikaeil.orfanian@toreforge.com>
2017-08-03 16:33:45 +0200

    add test for method adds an activity for a user to the activity db

--- tests/gateway/test_activity_gateway.py
+++ tests/gateway/test_activity_gateway.py
@@ -1,4 +1,4 @@
-from ...gateway.activity_gateway import ActivitySearch
+from ...gateway.activity_gateway import ActivitySearch, ActivityPersistor
 
 
 def test_correct_user_activities_are_found_when_user_has_multiple_activities(
@@ -13,3 +13,8 @@ def test_correct_user_activities_are_found_when_user_has_multiple_activities(
 def test_no_user_activities_are_found_when_user_has_no_activities(test_user):
     activities = ActivitySearch.user_activities(test_user)
     assert len(activities) == 0
+
+
+def test_new_activity_added_to_db(test_db, test_user, test_activity):
+    ActivityPersistor.add_new_activity_to_db(test_activity)
+    assert len(ActivitySearch.user_activities(test_user)) == 1
