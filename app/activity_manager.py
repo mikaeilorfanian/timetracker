03cf33f0505808ecb7dcfcdff8b7e1532c466110
Mikaeil Orfanian <mikaeil.orfanian@toreforge.com>
2017-08-03 11:59:41 +0200

    separate methods that check if there's any activity in progress or any activity with same category
    This creates two methods for 2 different use cases.

--- app/activity_manager.py
+++ app/activity_manager.py
@@ -10,7 +10,9 @@ class ActivityManager:
 
     @classmethod
     def start_new_activity(cls, category):
-        if cls.activity_with_same_category_already_started(category):
+        if cls.any_activity_already_started():
+            raise ActivityWithSameCategoryExistsError
+        elif cls.activity_with_same_category_already_started(category):
             raise ActivityWithSameCategoryExistsError
         else:
             a = Activity(category)
@@ -19,6 +21,14 @@ class ActivityManager:
 
     @classmethod
     def activity_with_same_category_already_started(cls, category):
+        in_progress_activities = ActivitySearch.in_progress_activities()
+        for activity in in_progress_activities:
+            if activity.category == category and activity.started:
+                return True
+        return False
+
+    @classmethod
+    def any_activity_already_started(cls):
         in_progress_activities = ActivitySearch.in_progress_activities()
         for activity in in_progress_activities:
             if activity.started:
