03542d753046b0cf0aa26b7715203241a4eec147
Mikaeil Orfanian <mikaeil.orfanian@toreforge.com>
2017-08-03 12:52:08 +0200

    activity gateway search now takes into account user id if activities

--- gateway/activity_gateway.py
+++ gateway/activity_gateway.py
@@ -4,5 +4,5 @@ from . import db
 class ActivitySearch:
 
     @classmethod
-    def in_progress_activities(cls):
-        return db['activities']
+    def in_progress_activities_for_user(cls, user):
+        return db['activities'].get(user._id, [])
