f5aedf7f41b3700b1ffe019c4458afa780fb2115
Mikaeil Orfanian <mikaeil.orfanian@toreforge.com>
2017-08-03 13:02:41 +0200

    added gateway for persisting new activities into db

--- gateway/activity_gateway.py
+++ gateway/activity_gateway.py
@@ -1,8 +1,20 @@
+from typing import List
+
 from . import db
+from ..entities.activity import Activity
+from ..entities.user import UserEntity
 
 
 class ActivitySearch:
 
     @classmethod
-    def in_progress_activities_for_user(cls, user):
+    def in_progress_activities_for_user(cls, user: UserEntity) -> List[Activity]:
         return db['activities'].get(user._id, [])
+
+
+class ActivityPersistor:
+
+    @classmethod
+    def persist_new_activity(cls, activity: Activity) -> None:
+        user_activities= db['activities'].get(activity.user._id, [])
+        user_activities.append(activity)
