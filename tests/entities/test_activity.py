6fee6915559a62f73fcda14720517916d35cdf7b
Mikaeil Orfanian <mikaeil.orfanian@toreforge.com>
2017-08-04 11:40:08 +0200

    refactor tests so they use test fixtures

--- tests/entities/test_activity.py
+++ tests/entities/test_activity.py
@@ -2,26 +2,20 @@ from ...entities.activity import Activity
 from ...entities.user import UserEntity
 
 
-def test_start_an_activity():
-    user = UserEntity('mike', _id=1)
-    a = Activity(user, 'working')
-    a.start()
-    assert a.started is True
-    assert a.started_at is not None
-    assert a.ended_at is None
+def test_start_an_activity(test_activity):
+    test_activity.start()
+    assert test_activity.started is True
+    assert test_activity.started_at is not None
+    assert test_activity.ended_at is None
 
 
-def test_end_an_activity():
-    user = UserEntity('mike', _id=1)
-    a = Activity(user, 'working')
-    a.start()
-    a.end()
-    assert a.started_at is not None
-    assert a.ended is True
-    assert a.ended_at is not None
+def test_end_an_activity(test_activity):
+    test_activity.start()
+    test_activity.end()
+    assert test_activity.started_at is not None
+    assert test_activity.ended is True
+    assert test_activity.ended_at is not None
 
 
-def test_activity_has_category():
-    user = UserEntity('mike', _id=1)
-    a = Activity(user, category='working')
-    assert a.category == 'working'
+def test_activity_has_category(test_activity):
+    assert test_activity.category is not None
