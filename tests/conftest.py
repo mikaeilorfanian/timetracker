64376aa006717ad2d6ff535e1a0fa65668301ace
Mikaeil Orfanian <mikaeil.orfanian@toreforge.com>
2017-08-04 13:28:51 +0200

    add new test fixture

--- tests/conftest.py
+++ tests/conftest.py
@@ -34,3 +34,19 @@ def test_activity(test_user):
     ActivityPersistor.add_new_activity_to_db(a)
     return a
 
+
+@pytest.fixture
+def test_user_with_multiple_activities_on_multiple_days(test_user, test_activity):
+    test_activity.end()
+
+    test_sleeping_activity = ActivityManager.start_new_activity(test_user, 'sleeping')
+    test_sleeping_activity.started_at = test_sleeping_activity.started_at.shift(days=-1)
+    ActivityPersistor.add_new_activity_to_db(test_sleeping_activity)
+    test_sleeping_activity.end()
+
+    test_working_activity = ActivityManager.start_new_activity(test_user, 'working')
+    test_working_activity.started_at = test_working_activity.started_at.shift(days=-2)
+    ActivityPersistor.add_new_activity_to_db(test_working_activity)
+    test_working_activity.end()
+
+    return test_user
