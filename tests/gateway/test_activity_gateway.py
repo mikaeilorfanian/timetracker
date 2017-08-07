4cfa576b4fb26a9d91d3ace56e3d4c4cb0e224ce
Mikaeil Orfanian <mikaeil.orfanian@toreforge.com>
2017-08-07 11:29:30 +0200

    grouped related tests into a test class to improve readability of tests

--- tests/gateway/test_activity_gateway.py
+++ tests/gateway/test_activity_gateway.py
@@ -1,20 +1,21 @@
 from ...gateway.activity_gateway import ActivitySearch, ActivityPersistor
 from ...use_cases.activity_manager import ActivityManager
 
+class TestSearchUserActivities:
 
-def test_correct_user_activities_are_found_when_user_has_multiple_activities(
-        test_user, test_sleeping_activity, test_working_activity, test_db):
+    def test_correct_user_activities_are_found_when_user_has_multiple_activities(
+            self, test_user, test_sleeping_activity, test_working_activity, test_db):
 
-    test_db['activities'][test_user._id] = [test_sleeping_activity,
-                                            test_working_activity]
-    activities = ActivitySearch.user_activities(test_user)
+        test_db['activities'][test_user._id] = [test_sleeping_activity,
+                                                test_working_activity]
+        activities = ActivitySearch.user_activities(test_user)
 
-    assert len(activities) == 2
+        assert len(activities) == 2
 
 
-def test_no_user_activities_are_found_when_user_has_no_activities(test_user):
-    activities = ActivitySearch.user_activities(test_user)
-    assert len(activities) == 0
+    def test_no_user_activities_are_found_when_user_has_no_activities(self, test_user):
+        activities = ActivitySearch.user_activities(test_user)
+        assert len(activities) == 0
 
 
 def test_new_activity_added_to_db(test_db, test_user, test_activity):
