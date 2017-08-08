dc207c614444ac31371e32af9dba0de4fbca5c53
Mikaeil Orfanian <mokt@outlook.com>
2017-08-08 13:11:59 +0200

    allow the test runner to run specific test files or all tests

--- scripts/run_tests.py
+++ scripts/run_tests.py
@@ -1,5 +1,13 @@
+import argparse
+import subprocess
+
 import prepare_for_tests
 
-import subprocess
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--path', default='.', help='path to the specific test you want to run, runs all tests by default')
+args = parser.parse_args()
+
+
 subprocess.run('pip install --editable .', shell=True)
-subprocess.call('pytest')
+subprocess.call('pytest %s' % args.path, shell=True)
