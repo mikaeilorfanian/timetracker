36702944ce34cbeeb38fc418b72065b402b8272c
Mikaeil Orfanian <mokt@outlook.com>
2017-08-08 14:02:47 +0200

    fix old tests which were breaking due to DB overhaul

--- tests/gateway/test_activity_gateway.py
+++ tests/gateway/test_activity_gateway.py
@@ -1,6 +1,6 @@
 import pytest
 
-from gateway.activity_gateway import ActivityGateway, ActivityPersistor, RecordNotFoundError
+from gateway.activity_gateway import ActivityGateway, RecordNotFoundError
 from use_cases.activity_manager import ActivityManager
 
 
@@ -9,7 +9,7 @@ class TestSearchUserActivities:
     def test_correct_user_activities_are_found_when_user_has_multiple_activities(
             self, test_sleeping_activity, test_working_activity, test_db):
 
-        test_db['activities'] = [test_sleeping_activity, test_working_activity]
+        test_db.data = [test_sleeping_activity, test_working_activity]
         activities = ActivityGateway.activities()
 
         assert len(activities) == 2
@@ -32,21 +32,22 @@ class TestSearchUserActivitiesStartedToday:
     def test_one_activity_returned_when_only_one_started_today(self, test_db, test_activity):
         todays_activities = ActivityGateway.activities_today()
         assert len(todays_activities) == 1
-        assert test_activity in todays_activities
+        assert test_activity._id == todays_activities[0]._id
 
     def test_user_has_activities_on_different_days_but_only_1_today(self, test_user_with_multiple_activities_on_multiple_days, test_activity, test_db):
         todays_activities = ActivityGateway.activities_today()
         assert len(todays_activities) == 1
-        assert test_activity in todays_activities
+        assert test_activity._id == todays_activities[0]._id
 
     def test_user_has_more_than_one_activity_today_and_more_on_other_days(self, test_user_with_multiple_activities_on_multiple_days, test_db, test_activity):
         test_working_activity = ActivityManager.start_new_activity('working')
-        ActivityPersistor.add_new_activity_to_db(test_working_activity)
+        ActivityGateway.add_new_activity_to_db(test_working_activity)
 
         todays_activities = ActivityGateway.activities_today()
         assert len(todays_activities) == 2
-        assert test_activity in todays_activities
-        assert test_working_activity in todays_activities
+        _ids = [a._id for a in todays_activities]
+        assert test_activity._id in _ids
+        assert test_working_activity._id in _ids
 
 
 class TestSearchForUserActivitiesWithSpecificCategoryStartedToday:
@@ -57,20 +58,20 @@ class TestSearchForUserActivitiesWithSpecificCategoryStartedToday:
     def test_one_activity_returned_when_only_one_started_today(self, test_db, test_activity):
         todays_activities = ActivityGateway.activities_today_in_this_category(test_activity.category)
         assert len(todays_activities) == 1
-        assert test_activity in todays_activities
+        assert test_activity._id == todays_activities[0]._id
 
     def test_user_has_activities_on_different_days_but_only_1_today(self, test_user_with_multiple_activities_on_multiple_days, test_activity, test_db):
         todays_activities = ActivityGateway.activities_today_in_this_category(test_activity.category)
         assert len(todays_activities) == 1
-        assert test_activity in todays_activities
+        assert test_activity._id == todays_activities[0]._id
 
     def test_user_has_more_than_one_activity_today_but_only_one_is_what_we_want_and_more_activities_on_other_days(self, test_user_with_multiple_activities_on_multiple_days, test_db, test_activity):
         test_working_activity = ActivityManager.start_new_activity('working')
-        ActivityPersistor.add_new_activity_to_db(test_working_activity)
+        ActivityGateway.add_new_activity_to_db(test_working_activity)
 
         todays_activities = ActivityGateway.activities_today_in_this_category('working')
         assert len(todays_activities) == 1
-        assert test_working_activity in todays_activities
+        assert test_working_activity._id == todays_activities[0]._id
 
 
 class TestSearchForActivity:
