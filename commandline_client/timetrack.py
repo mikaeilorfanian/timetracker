03676db2376f2ba8d1f1c4b735b8ae3571a1f1f0
Mikaeil Orfanian <mokt@outlook.com>
2017-08-09 11:27:15 +0200

    #5 Print out how much time user spent on activity when starting new activity

--- commandline_client/timetrack.py
+++ commandline_client/timetrack.py
@@ -1,11 +1,24 @@
 import click
 
+from use_cases.activity_manager import ActivityManager
+from use_cases.activity_report import format_seconds_returnbed_by_report, TimeSpentInCategoryReport
+
 
 @click.command()
-@click.argument('activity', default='-', required=True)
+@click.argument('activity', required=True)
 def cli(activity):
     """
     Track how much time you spend on different activities throughout the day!
     ACTIVITY is the type of activity you want to start tracking. Examples: working, reading, studying.
     """
-    click.echo('Started tracking time for %s' % activity)
+
+    a = ActivityManager.start_tracking_new_activity(activity)
+    click.echo('Started tracking "%s".' % a.category)
+    click.echo(
+        "So far, you've spent {} on {} today!".format(
+            format_seconds_returnbed_by_report(
+                TimeSpentInCategoryReport.generate_for_this_category_of_activity(activity)[activity]
+            ),
+            activity
+        )
+    )
