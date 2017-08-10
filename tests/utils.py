6056d1ddd19d75dcb84a1b0fda34d095408e0340
Mikaeil Orfanian <mokt@outlook.com>
2017-08-10 17:25:15 +0200

    shift the start date back when the activity is start today
    when start_at is utcnow() and end_at is start_at+some_seconds, then the length method returns 0, to avoid this the above change is needed

--- tests/utils.py
+++ tests/utils.py
@@ -3,8 +3,13 @@ from use_cases.activity_manager import ActivityManager
 
 
 def make_activity(started_days_ago, length_in_seconds, category):
+    if started_days_ago == 0:
+        seconds_to_shift = length_in_seconds
+    else:
+        seconds_to_shift = 0
+
     act1 = ActivityManager.start_tracking_new_activity(category)
-    act1.started_at = act1.started_at.shift(days=-started_days_ago)
+    act1.started_at = act1.started_at.shift(days=-started_days_ago, seconds=-seconds_to_shift)
     act1.end()
     act1.ended_at = act1.started_at.shift(seconds=length_in_seconds)
     ActivityGateway.update_activity_in_db(act1)
